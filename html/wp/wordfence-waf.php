cd68b05473f6dcea9c55f9dc5862d670107a58de
Joshua Fredrickson <joshua@philandcompany.com>
2019-03-25 19:41:43 -0400

    Add wordfence-waf.php to wp/

---
+++ html/wp/wordfence-waf.php
@@ -0,0 +1,9 @@
+<?php
+// Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
+
+if (file_exists('/home/jcfnyorg/jcfny.org/html/app/plugins/wordfence/waf/bootstrap.php')) {
+        define("WFWAF_LOG_PATH", '/home/jcfnyorg/jcfny.org/html/app/wflogs/');
+        include_once '/home/jcfnyorg/jcfny.org/html/app/plugins/wordfence/waf/bootstrap.php';
+}
+?>
+
