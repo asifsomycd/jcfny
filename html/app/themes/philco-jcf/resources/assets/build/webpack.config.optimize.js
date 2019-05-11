6d4ea610d47982a5d03a068bcd75784a86d5f010
Joshua Fredrickson <joshua@orangepineapple.com>
2019-05-11 18:18:37 -0500

    Enable uglifyjs cache in webpack optimize

--- html/app/themes/philco-jcf/resources/assets/build/webpack.config.optimize.js
+++ html/app/themes/philco-jcf/resources/assets/build/webpack.config.optimize.js
@@ -1,35 +1,37 @@
-'use strict'; // eslint-disable-line
+"use strict"; // eslint-disable-line
 
-const { default: ImageminPlugin } = require('imagemin-webpack-plugin');
-const imageminMozjpeg = require('imagemin-mozjpeg');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const { default: ImageminPlugin } = require("imagemin-webpack-plugin");
+const imageminMozjpeg = require("imagemin-mozjpeg");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
-const config = require('./config');
+const config = require("./config");
 
 module.exports = {
   plugins: [
     new ImageminPlugin({
       optipng: { optimizationLevel: 2 },
       gifsicle: { optimizationLevel: 3 },
-      pngquant: { quality: '65-90', speed: 4 },
+      pngquant: { quality: "65-90", speed: 4 },
       svgo: {
         plugins: [
           { removeUnknownsAndDefaults: false },
           { cleanupIDs: false },
-          { removeViewBox: false },
-        ],
+          { removeViewBox: false }
+        ]
       },
       plugins: [imageminMozjpeg({ quality: 75 })],
-      disable: (config.enabled.watcher),
+      disable: config.enabled.watcher
     }),
     new UglifyJsPlugin({
       uglifyOptions: {
         ecma: 5,
         compress: {
           warnings: true,
-          drop_console: true,
-        },
+          drop_console: true
+        }
       },
-    }),
-  ],
+      cache: true,
+      parallel: 4
+    })
+  ]
 };
