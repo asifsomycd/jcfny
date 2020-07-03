f1de5259a14173238cefeb854149710d62e19bef
Joshua Fredrickson <joshua@orangepineapple.com>
2020-07-03 16:49:33 -0500

    ♻️ Make compatible with oscarotero/env 2.x

--- config/application.php
+++ config/application.php
@@ -9,17 +9,21 @@
  */
 
 use Roots\WPConfig\Config;
-
-/** @var string Directory containing all of the site's files */
-$root_dir = dirname(__DIR__);
-
-/** @var string Document Root */
-$webroot_dir = $root_dir . '/html';
+use function Env\env;
 
 /**
- * Expose global env() function from oscarotero/env
+ * Directory containing all of the site's files
+ *
+ * @var string
  */
-Env::init();
+$root_dir = dirname(__DIR__);
+
+/**
+ * Document Root
+ *
+ * @var string
+ */
+$webroot_dir = $root_dir . '/html';
 
 /**
  * Use Dotenv to set required environment variables and load .env file in root
