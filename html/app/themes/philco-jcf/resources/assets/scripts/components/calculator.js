71f1ce5507e6b79b0beb0094b369fb7e763b70b6
Joshua Fredrickson <joshua@orangepineapple.com>
2020-09-24 15:38:15 -0500

    Code formatting: calculator.js

--- html/app/themes/philco-jcf/resources/assets/scripts/components/calculator.js
+++ html/app/themes/philco-jcf/resources/assets/scripts/components/calculator.js
@@ -2,9 +2,9 @@
  * Fee Calculator
  */
 /* global calculatorParams */
-import { IMaskInput } from 'react-imask';
-import { SlideDown } from 'react-slidedown';
-import NumberDisplay from './numberDisplay';
+import { IMaskInput } from "react-imask";
+import { SlideDown } from "react-slidedown";
+import NumberDisplay from "./numberDisplay";
 
 function Calculator() {
   const [formValues, setFormValues] = wp.element.useState({
@@ -71,9 +71,11 @@ function Calculator() {
 
     if (!newFormResults.disclaimer && formValues.balance) {
       newFormResults.feeMonth = newFormResults.feeAnnual / 12;
-      newFormResults.returnAnnual = formValues.balance * calculatorParams.multiplier;
+      newFormResults.returnAnnual =
+        formValues.balance * calculatorParams.multiplier;
       newFormResults.returnMonth = newFormResults.returnAnnual / 12;
-      newFormResults.growth = newFormResults.returnAnnual - newFormResults.feeAnnual;
+      newFormResults.growth =
+        newFormResults.returnAnnual - newFormResults.feeAnnual;
       newFormResults.display = true;
     }
 
@@ -86,23 +88,26 @@ function Calculator() {
   wp.element.useEffect(() => {
     const handleResize = () => {
       const newHeight = calculatorForm.current.clientHeight;
-      calculatorFormBackfill.current.setAttribute('style', `height: ${newHeight}px;`);
+      calculatorFormBackfill.current.setAttribute(
+        "style",
+        `height: ${newHeight}px;`
+      );
     };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     handleResize(); // Run it on first load too.
 
-    return () => window.removeEventListener('resize', handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   });
 
   /**
    * Balance is updated
    * Trigger state change
    */
-  const handleBalanceChange = unmaskedValue => {
+  const handleBalanceChange = (unmaskedValue) => {
     setFormValues({
       ...formValues,
-      ['balance']: unmaskedValue,
+      ["balance"]: unmaskedValue,
     });
   };
 
@@ -110,170 +115,246 @@ function Calculator() {
    * Form values are updated
    * Trigger state change
    */
-  const handleChildrensChange = event => {
+  const handleChildrensChange = (event) => {
     const name = event.target.name;
     let value = event.target.value;
 
     setFormValues({
       ...formValues,
-      [name]: value === 'y' ? true : false,
+      [name]: value === "y" ? true : false,
     });
   };
 
   /**
    * Scroll to results
    */
-  const handleCalculateClick = event => {
+  const handleCalculateClick = (event) => {
     event.preventDefault();
     results.current.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
+      behavior: "smooth",
+      block: "start",
     });
   };
 
   return (
-    <div className='calculator'>
-      <div className='calculator__row-1'>
-        <div ref={calculatorFormBackfill} className='calculator__backfill calculator__backfill--left' />
-        <div className='calculator__backfill d-none d-md-block'>
+    <div className="calculator">
+      <div className="calculator__row-1">
+        <div
+          ref={calculatorFormBackfill}
+          className="calculator__backfill calculator__backfill--left"
+        />
+        <div className="calculator__backfill d-none d-md-block">
           {calculatorParams && (
-            <div className='row h-100'>
-              <div className='calculator__image d-none d-lg-block col-5 ml-auto' style={{ backgroundImage: `url(${calculatorParams.image})` }} />
+            <div className="row h-100">
+              <div
+                className="calculator__image d-none d-lg-block col-5 ml-auto"
+                style={{ backgroundImage: `url(${calculatorParams.image})` }}
+              />
             </div>
           )}
         </div>
-        <div className='container'>
-          <div className='row'>
-            <div ref={calculatorForm} className='calculator__form col-12 col-md-6 px-0 pr-md-5 pl-md-3 wrapper--blueberry'>
-              <div className='wrapper--blueberry px-3 px-md-0 py-0'>
-                <h2>{calculatorParams.title ? calculatorParams.title : 'Fee Calculator'}</h2>
-                <div className='form-group'>
-                  <label htmlFor='balance'>
+        <div className="container">
+          <div className="row">
+            <div
+              ref={calculatorForm}
+              className="calculator__form col-12 col-md-6 px-0 pr-md-5 pl-md-3 wrapper--blueberry"
+            >
+              <div className="wrapper--blueberry px-3 px-md-0 py-0">
+                <h2>
+                  {calculatorParams.title
+                    ? calculatorParams.title
+                    : "Fee Calculator"}
+                </h2>
+                <div className="form-group">
+                  <label htmlFor="balance">
                     <strong>Anticipated Fund Balance:</strong>
                   </label>
-                  <div className='input-group money'>
-                    <div className='input-group-prepend'>
-                      <div className='input-group-text'>$</div>
+                  <div className="input-group money">
+                    <div className="input-group-prepend">
+                      <div className="input-group-text">$</div>
                     </div>
                     <IMaskInput
-                      id='balance'
-                      name='balance'
-                      className='form-control'
+                      id="balance"
+                      name="balance"
+                      className="form-control"
                       mask={Number}
-                      radix='.'
+                      radix="."
                       unmask={true}
                       max={1000000000}
-                      thousandsSeparator=','
+                      thousandsSeparator=","
                       onAccept={handleBalanceChange}
                     />
                   </div>
-                  {formResults.disclaimer && <small className='form-text text-white'>{formResults.disclaimer}</small>}
+                  {formResults.disclaimer && (
+                    <small className="form-text text-white">
+                      {formResults.disclaimer}
+                    </small>
+                  )}
                 </div>
                 {calculatorParams.showChildrensFund && (
                   <div>
-                    <div className='form-group'>
-                      <strong>Is it a Children&apos;s Giving Fund or a Bar/Bat Mitzvah Fund?</strong>
+                    <div className="form-group">
+                      <strong>
+                        Is it a Children&apos;s Giving Fund or a Bar/Bat Mitzvah
+                        Fund?
+                      </strong>
                     </div>
-                    <div className='form-check'>
+                    <div className="form-check">
                       <input
-                        id='childrensFundY'
-                        type='radio'
-                        name='childrensFund'
-                        value='y'
-                        className='form-check-input'
+                        id="childrensFundY"
+                        type="radio"
+                        name="childrensFund"
+                        value="y"
+                        className="form-check-input"
                         onChange={handleChildrensChange}
                       />
-                      <label htmlFor='childrensFundY' className='form-check-label'>
+                      <label
+                        htmlFor="childrensFundY"
+                        className="form-check-label"
+                      >
                         Yes
                       </label>
                     </div>
-                    <div className='form-check mb-3'>
-                      <input id='childrensFundN' type='radio' name='childrensFund' value='n' className='form-check-input' defaultChecked onChange={handleChildrensChange} />
-                      <label htmlFor='childrensFundN' className='form-check-label'>
+                    <div className="form-check mb-3">
+                      <input
+                        id="childrensFundN"
+                        type="radio"
+                        name="childrensFund"
+                        value="n"
+                        className="form-check-input"
+                        defaultChecked
+                        onChange={handleChildrensChange}
+                      />
+                      <label
+                        htmlFor="childrensFundN"
+                        className="form-check-label"
+                      >
                         No
                       </label>
                     </div>
                   </div>
                 )}
-                <button type='submit' className='btn btn-outline-light' onClick={handleCalculateClick} disabled={!formResults.display}>
+                <button
+                  type="submit"
+                  className="btn btn-outline-light"
+                  onClick={handleCalculateClick}
+                  disabled={!formResults.display}
+                >
                   Calculate Fee
                 </button>
               </div>
             </div>
-            <div ref={results} className='calculator__results col-12 col-md-6'>
-              <div className='row'>
-                <div className='wrapper--white col-9 px-3 pl-md-5 pr-md-3'>
+            <div ref={results} className="calculator__results col-12 col-md-6">
+              <div className="row">
+                <div className="wrapper--white col-9 px-3 pl-md-5 pr-md-3">
                   {formResults.display && (
                     <div>
                       <h3>Estimated Fees</h3>
-                      <div className='text-blueberry lead'>
-                        <span className='h1'>
-                          <NumberDisplay value={formResults.feeMonth} digits={2} prepend='$' />
+                      <div className="text-blueberry lead">
+                        <span className="h1">
+                          <NumberDisplay
+                            value={formResults.feeMonth}
+                            digits={2}
+                            prepend="$"
+                          />
                         </span>
-                        <strong className='ml-2 text-nowrap'>per month</strong>
+                        <strong className="ml-2 text-nowrap">per month</strong>
                       </div>
-                      <div className='text-blueberry lead'>
+                      <div className="text-blueberry lead">
                         <strong>or</strong>
-                        <span className='h2 ml-1'>
-                          <NumberDisplay value={formResults.feeAnnual} prepend='$' />
+                        <span className="h2 ml-1">
+                          <NumberDisplay
+                            value={formResults.feeAnnual}
+                            prepend="$"
+                          />
                         </span>
-                        <strong className='ml-1 text-nowrap'>per year</strong>
+                        <strong className="ml-1 text-nowrap">per year</strong>
                       </div>
                     </div>
                   )}
-                  {!formResults.display && <div className='calculator__placeholder h2'>{calculatorParams.text}</div>}
+                  {!formResults.display && (
+                    <div className="calculator__placeholder h2">
+                      {calculatorParams.text}
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div className='calculator__row-2'>
-        <SlideDown className={'my-dropdown-slidedown'}>
+      <div className="calculator__row-2">
+        <SlideDown className={"my-dropdown-slidedown"}>
           {formResults.display && formResults.growth > 0 ? (
-            <div className='wrapper--pale-gray'>
-              <div className='container'>
-                <div className='row'>
-                  <div className='col-12 col-md-10'>
-                    <h2 className='mb-2'>{calculatorParams.results.title}</h2>
-                    <h4 className='mb-4'>{calculatorParams.results.subtitle}</h4>
-                    <div className='columns--2 mb-5' dangerouslySetInnerHTML={{ __html: calculatorParams.results.text }} />
+            <div className="wrapper--pale-gray">
+              <div className="container">
+                <div className="row">
+                  <div className="col-12 col-md-10">
+                    <h2 className="mb-2">{calculatorParams.results.title}</h2>
+                    <h4 className="mb-4">
+                      {calculatorParams.results.subtitle}
+                    </h4>
+                    <div
+                      className="columns--2 mb-5"
+                      dangerouslySetInnerHTML={{
+                        __html: calculatorParams.results.text,
+                      }}
+                    />
                   </div>
                 </div>
 
-                <div className='row'>
-                  <div className='col-12 col-md-5 pr-md-5'>
-                    <h3 className='h2'>Estimated Investment Returns:</h3>
-                    <p className='text-blueberry h4'>
-                      <span className='text-tealish'>+</span>
-                      <span className='text-tealish h2 mr-1'>
-                        <NumberDisplay value={formResults.returnMonth} digits={2} prepend='$' />
+                <div className="row">
+                  <div className="col-12 col-md-5 pr-md-5">
+                    <h3 className="h2">Estimated Investment Returns:</h3>
+                    <p className="text-blueberry h4">
+                      <span className="text-tealish">+</span>
+                      <span className="text-tealish h2 mr-1">
+                        <NumberDisplay
+                          value={formResults.returnMonth}
+                          digits={2}
+                          prepend="$"
+                        />
                       </span>
                       per month
                       <br />
-                      or <span className='text-tealish'>+</span>
-                      <span className='text-tealish h2 mr-1'>
-                        <NumberDisplay value={formResults.returnAnnual} prepend='$' />
+                      or <span className="text-tealish">+</span>
+                      <span className="text-tealish h2 mr-1">
+                        <NumberDisplay
+                          value={formResults.returnAnnual}
+                          prepend="$"
+                        />
                       </span>
                       per year
                     </p>
-                    <div className='text-sm' dangerouslySetInnerHTML={{ __html: calculatorParams.results.returnsDisclaimer }} />
+                    <div
+                      className="text-sm"
+                      dangerouslySetInnerHTML={{
+                        __html: calculatorParams.results.returnsDisclaimer,
+                      }}
+                    />
                   </div>
-                  <div className='calculator__chevron col-12 col-md-1 pt-2 text-blueberry text-center mb-3'>
-                    <i className='far fa-chevron-right fa-2x'></i>
+                  <div className="calculator__chevron col-12 col-md-1 pt-2 text-blueberry text-center mb-3">
+                    <i className="far fa-chevron-right fa-2x"></i>
                   </div>
-                  <div className='col-12 col-md-5 pl-md-5'>
-                    <h3 className='h2'>Net Growth of Your JCF Donor Advised Fund:</h3>
-                    <p className='text-blueberry h4'>
-                      <span className='text-tealish'>+</span>
-                      <span className='text-tealish h1 mr-1'>
-                        <NumberDisplay value={formResults.growth} prepend='$' />
+                  <div className="col-12 col-md-5 pl-md-5">
+                    <h3 className="h2">
+                      Net Growth of Your JCF Donor Advised Fund:
+                    </h3>
+                    <p className="text-blueberry h4">
+                      <span className="text-tealish">+</span>
+                      <span className="text-tealish h1 mr-1">
+                        <NumberDisplay value={formResults.growth} prepend="$" />
                       </span>
-                      <span className='text-tealish'>per year</span>
+                      <span className="text-tealish">per year</span>
                       <br />
                       in net funds for giving
                     </p>
-                    <div className='text-sm' dangerouslySetInnerHTML={{ __html: calculatorParams.results.growthDisclaimer }} />
+                    <div
+                      className="text-sm"
+                      dangerouslySetInnerHTML={{
+                        __html: calculatorParams.results.growthDisclaimer,
+                      }}
+                    />
                   </div>
                 </div>
               </div>
@@ -288,6 +369,9 @@ function Calculator() {
 /**
  * Only if the calculator variable is set (json object from template)
  */
-if (typeof calculatorParams !== 'undefined') {
-  wp.element.render(<Calculator />, document.querySelector('#react-calculator'));
+if (typeof calculatorParams !== "undefined") {
+  wp.element.render(
+    <Calculator />,
+    document.querySelector("#react-calculator")
+  );
 }
