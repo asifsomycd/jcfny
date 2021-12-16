6463127a79b6b4b64369c031138a08839351de46
Joshua Fredrickson <joshua@orangepineapple.com>
2021-12-16 15:27:35 -0600

    Reinstate ability to hide footer form; hide footer form on app pages

--- html/app/themes/philco-jcf/resources/views/partials/footer-default.blade.php
+++ html/app/themes/philco-jcf/resources/views/partials/footer-default.blade.php
@@ -16,11 +16,13 @@
 			</div>
 
 			<div class="col-12 col-lg-5 col-xl-6 mt-5 mt-lg-0">
-				<div class="footer-form d-none d-md-block">
-					@include('partials.action-form', [
-						'form_display' => 'inline',
-					] )
-				</div>
+        @if ($show_footer_form && ! $application_form)
+          <div class="footer-form d-none d-md-block mb-md-5">
+            @include('partials.action-form', [
+              'form_display' => 'inline',
+            ] )
+          </div>
+        @endif
 
         @include('partials.social')
 			</div>
