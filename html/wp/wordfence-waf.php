989dd9054dbf5cae1d477e85374dcb6d4c9ad6c1
Joshua Fredrickson <joshua@reciprocal.nyc>
2022-04-16 16:24:59 -0500

    Change home path for Wordfence

--- html/wp/wordfence-waf.php
+++ html/wp/wordfence-waf.php
@@ -1,7 +1,7 @@
 <?php
 // Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
 
-if (file_exists('/home/jcfnyorg/jcfny.org/html/app/plugins/wordfence/waf/bootstrap.php')) {
-        define("WFWAF_LOG_PATH", '/home/jcfnyorg/jcfny.org/html/app/wflogs/');
-        include_once '/home/jcfnyorg/jcfny.org/html/app/plugins/wordfence/waf/bootstrap.php';
+if (file_exists('/chroot/home/a0d15d27/cbaf7970ac.nxcli.net/html/app/plugins/wordfence/waf/bootstrap.php')) {
+    define("WFWAF_LOG_PATH", '/chroot/home/a0d15d27/cbaf7970ac.nxcli.net/html/app/wflogs/');
+    include_once '/chroot/home/a0d15d27/cbaf7970ac.nxcli.net/html/app/plugins/wordfence/waf/bootstrap.php';
 }
