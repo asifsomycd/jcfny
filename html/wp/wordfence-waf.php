ad67bc03896896b041cbac613ca288293dc1c1f6
Joshua Fredrickson <joshua@reciprocal.nyc>
2022-04-16 17:52:38 -0500

    Use relative paths

--- html/wp/wordfence-waf.php
+++ html/wp/wordfence-waf.php
@@ -1,7 +1,7 @@
 <?php
 // Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
 
-if (file_exists('/chroot/home/a0d15d27/cbaf7970ac.nxcli.net/html/app/plugins/wordfence/waf/bootstrap.php')) {
-    define("WFWAF_LOG_PATH", '/chroot/home/a0d15d27/cbaf7970ac.nxcli.net/html/app/wflogs/');
-    include_once '/chroot/home/a0d15d27/cbaf7970ac.nxcli.net/html/app/plugins/wordfence/waf/bootstrap.php';
+if (file_exists(__DIR__.'/../app/plugins/wordfence/waf/bootstrap.php')) {
+    define("WFWAF_LOG_PATH", __DIR__.'/../app/wflogs/');
+    include_once __DIR__.'/../app/plugins/wordfence/waf/bootstrap.php';
 }
