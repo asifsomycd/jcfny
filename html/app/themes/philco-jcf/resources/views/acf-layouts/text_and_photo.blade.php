03b63df48a4435aeed1c4d455fabd00234e6c352
Joshua Fredrickson <joshua@reciprocal.nyc>
2022-05-07 17:07:57 -0500

    Add column width selection to text_and_photo

--- html/app/themes/philco-jcf/resources/views/acf-layouts/text_and_photo.blade.php
+++ html/app/themes/philco-jcf/resources/views/acf-layouts/text_and_photo.blade.php
@@ -1,10 +1,18 @@
 <div class="layout-block__text-and-photo position-relative d-flex flex-column" data-scroll>
   <div class="layout-block__text-and-photo_text container order-1 order-lg-0">
-    <div class="row align-items-center {{ $image_options['position'] === 'left' ? 'justify-content-end' : 'justify-content-start' }}">
-      <div class="col-12 col-lg-4 font-size-md pt-lg-5 pb-5">
+    <div class="
+      row align-items-center
+      {{ $image_options['position'] === 'left' ? 'justify-content-end' : 'justify-content-start' }}
+    ">
+      <div class="
+        col-12 font-size-md pt-lg-5 pb-5
+        {{ $image_options['width'] === '7' ? 'col-lg-4' : 'col-lg-5' }}
+      ">
         <div class="wrapper">
           <div class="bg-white mt-n5 mt-lg-0 px-4 px-lg-0 pt-4 pt-lg-0">
-            <{{ $h1 ? 'h1' : 'h2' }}>{!! $title !!}</{{ $h1 ? 'h1' : 'h2' }}>
+            <{{ $h1 ? 'h1' : 'h2' }}>
+              {!! $title !!}
+            </{{ $h1 ? 'h1' : 'h2' }}>
             {!! $text !!}
           </div>
 
@@ -29,9 +37,13 @@
 
   <div class="layout-block__text-and-photo_container order-0 order-lg-1">
     <div class="container-fluid h-100">
-      <div class="row h-100 {{ $image_options['position'] === 'left' ? 'justify-content-start' : 'justify-content-end' }}">
+      <div class="
+        row h-100
+        {{ $image_options['position'] === 'left' ? 'justify-content-start' : 'justify-content-end' }}
+      ">
         <div class="
-            layout-block__text-and-photo_block col-12 col-lg-7 h-100
+            layout-block__text-and-photo_block col-12 h-100
+            {{ $image_options['width'] === '7' ? 'col-lg-7' : 'col-lg-6' }}
             {{ $image_options['fit'] === 'cover' ? 'photo--cover' : 'photo--contain' }}
           "
           style="background-image: url({{ $image['sizes']['col-6'] }});"
