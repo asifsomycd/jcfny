4a84e465681cd53af1873ae8df698c7c8985f235
Joshua Fredrickson <joshua@reciprocal.nyc>
2022-09-14 18:42:54 -0500

    Update favicons

--- html/app/themes/philco-jcf/resources/views/partials/head.blade.php
+++ html/app/themes/philco-jcf/resources/views/partials/head.blade.php
@@ -2,13 +2,16 @@
 	<meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="apple-touch-icon" sizes="180x180" href="{{ home_url('/favicons/apple-touch-icon.png') }}">
-	<link rel="icon" type="image/png" sizes="32x32" href="{{ home_url('/favicons/favicon-32x32.png') }}">
-	<link rel="icon" type="image/png" sizes="16x16" href="{{ home_url('/favicons/favicon-16x16.png') }}">
-	<link rel="manifest" href="{{ home_url('/site.webmanifest') }}">
-	<link rel="mask-icon" href="{{ home_url('/favicons/safari-pinned-tab.svg') }}" color="#815c9e">
-	<meta name="msapplication-TileColor" content="#815c9e">
-	<meta name="theme-color" content="#815c9e">
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ home_url('/favicons/apple-touch-icon.png') }}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ home_url('/favicons/favicon-32x32.png') }}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ home_url('/favicons/favicon-16x16.png') }}">
+  <link rel="manifest" href="{{ home_url('/site.webmanifest') }}">
+  <link rel="mask-icon" href="{{ home_url('/favicons/safari-pinned-tab.svg') }}" color="#230761">
+  <link rel="shortcut icon" href="{{ home_url('/favicons/favicon.ico') }}">
+  <meta name="msapplication-TileColor" content="#e8effa">
+  <meta name="msapplication-config" content="{{ home_url('/browserconfig.xml') }}">
+  <meta name="theme-color" content="#ffffff">
+
 	@php wp_head() @endphp
 	@if ( $site_header_scripts )
 		@foreach ( $site_header_scripts as $script )
