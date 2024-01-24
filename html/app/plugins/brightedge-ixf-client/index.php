41f1077ea93eb9f9fa02115580b943ba29004bcc
Joshua Fredrickson <joshua@reciprocal.nyc>
2024-01-23 18:09:08 -0600

    Bump Brightedge IXF to 1.5.12

--- html/app/plugins/brightedge-ixf-client/index.php
+++ html/app/plugins/brightedge-ixf-client/index.php
@@ -6,7 +6,7 @@
  * License:     GNU General Public License v2
  * Plugin Name: BrightEdge IXF Client Interface
  * Plugin URI:  https://brightedge.com
- * Version:     1.4.18
+ * Version:     1.5.12
  * Description: Custom integration with BrightEdge.
  */
 
@@ -16,11 +16,12 @@
 //Infrastructure Product Terms.
 
 //IXF: save the be_ixf_client.php file to your server, then use "require" to include it in your template.
-require 'brightedge-ixf-client.php';
+require 'be_ixf_client.php';
+
 use BrightEdge\BEIXFClient;
 
 //IXF: the following array and constructor must be placed before any HTML is written to the page.
-$be_ixf_config = array(
+$be_ixf = new BEIXFClient([
     BEIXFClient::$CAPSULE_MODE_CONFIG => BEIXFClient::$REMOTE_PROD_CAPSULE_MODE,
     BEIXFClient::$ACCOUNT_ID_CONFIG => "f00000000154546",
 
@@ -32,5 +33,4 @@ $be_ixf_config = array(
     // page content.  Add them to this config value, separated by the pipe character (|).
     BEIXFClient::$WHITELIST_PARAMETER_LIST_CONFIG => "ixf",
 
-);
-$be_ixf = new BEIXFClient($be_ixf_config);
+]);
