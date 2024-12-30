84411d9d93a793fc31bdc4fe1cfdb214af46e1fe
Joshua Fredrickson <joshua@reciprocal.nyc>
2024-12-30 12:58:26 -0600

    Force form to show (and work!)

--- html/app/themes/philco-jcf/resources/views/template-application-form.blade.php
+++ html/app/themes/philco-jcf/resources/views/template-application-form.blade.php
@@ -122,11 +122,14 @@
   <div class="application-form">
     {!! $application_form !!}
     {{--
-      Force form to show
+      Force form to show. Has to be some weird JS timing issue with the form.
       @see {https://stackoverflow.com/a/67741301/1331870}
     --}}
     <script>
-      jQuery('.gform_wrapper').show();
+      window.document.dispatchEvent(new Event("DOMContentLoaded", {
+        bubbles: true,
+        cancelable: true
+      }));
     </script>
   </div>
 @endsection
